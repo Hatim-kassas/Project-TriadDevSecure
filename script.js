78acfff5d4d2da0909e93dbe48f27ae6c3b8db5b
Yahya <yahyaouerdighi2017@gmail.com>
2024-10-06 19:20:25 +0100

    service & additional section created at 10/6/2024 by yahya

--- script.js
+++ script.js
@@ -8,27 +8,3 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('scrolled')
     }
 })
-
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const sections = document.querySelectorAll('.section');
-
-        const options = {
-            root: null,
-            threshold: 0.1
-        };
-
-        const observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add('visible'); // Add a visible class
-                    observer.unobserve(entry.target); // Stop observing once animated
-                }
-            });
-        }, options);
-
-        sections.forEach(section => {
-            observer.observe(section);
-        });
-    });
-</script>
