a1924bf8a94958ddd8c207e5dd7bf99eab21dee4
Yahya <yahyaouerdighi2017@gmail.com>
2024-10-06 20:52:41 +0100

    new sections

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('scrolled')
     }
 })
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+}
