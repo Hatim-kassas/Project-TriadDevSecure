cfbcbe20b3751e1409843d6fbfbc7901fd7509fc
Hatim-kassas <hatim.kassas20@gmail.com>
2024-11-10 01:02:23 +0100

    Update add file php and connect form with database

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db = 'contact_form';
+
+    // Create a connection
+    $conn = new mysqli($host, $user, $pass, $db);
+
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+?>
---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>
+            TriadDevSecure
+        </title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <main>
+            <section class="main-section" id="home">
+                <header>
+                    <nav class="navbar">
+                        <!-- Logo Section -->
+                        <div class="logo">
+                            <img src="assets/images/Logo/logo 1.png" alt="Logo">
+                        </div>
+
+                        <!-- Hamburger Menu Icon -->
+                        <div class="hamburger" onclick="toggleMenu()">
+                            <div class="bar"></div>
+                            <div class="bar"></div>
+                            <div class="bar"></div>
+                        </div>
+
+                        <!-- Navigation Links -->
+                        <ul class="nav-links">
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#about">About</a></li>
+                            <li><a href="#services">Services</a></li>
+                            <li><a href="#Process">Our Process</a></li>
+                            <li><a href="#ChooseUS">Why Choose US</a></li>
+                        </ul>
+
+                        <!-- Social Media Buttons -->
+                        <div class="social-media">
+                            <a href="https://www.instagram.com" target="_blank" class="social-btn">
+                                <img src="assets/images/social/insta.png" alt="Instagram">
+                            </a>
+                            <a href="https://www.linkedin.com" target="_blank" class="social-btn">
+                                <img src="assets/images/social/in.png" alt="LinkedIn">
+                            </a>
+                            <a href="https://www.github.com" target="_blank" class="social-btn">
+                                <img src="assets/images/social/x.png" alt="X">
+                            </a>
+                        </div>
+                    </nav>
+                </header>
+
+
+                <div class="content-wrapper">
+                    <div class="vector">
+                        <img src="assets/images/vector/vector.png" alt="Vector Image">
+                    </div>
+                    <div class="text-content">
+                        <p class="welcome-title" id="welcome-title-id">Welcome To TriadDevSecure</p>
+                        <h1 id="typing-effect">Seamless Development <br> Strong Security</h1>
+                        <div class="welcome-text">
+                            <p class="description">We Empower Businesses To Thrive In The Digital Age</p>
+                            <p class="description">With Cutting-Edge Solutions, From Intuitive User</p>
+                            <p class="description">Interfaces To Secure Cloud Migrations And Data-Driven</p>
+                            <p class="description">Marketing Strategies. Let Us Drive Your Digital Success.</p>
+                        </div>
+                        <a href="#contact" style="text-decoration: none;"><button class="contact-btn">Contact Us</button></a>
+                    </div>
+                </div>
+            </section>
+
+
+            <section class="additional-info-slogan">
+                <div class="info-content-slogan">
+                    <h3>Driving Digital Success with Development & Security</h3>
+                </div>
+            </section>
+
+
+            <section class="empowering-section" id="about">
+                <div class="content-container">
+                    <h3>Empowering Growth with Secure Development</h3>
+                    <p>
+                        We Deliver Cutting-Edge Development Solutions With A Strong Emphasis On Security. <br> We Excel In Seamless Backend Development Using Java, Full-Stack Solutions With Flutter And PHP Laravel, And Intuitive UI/UX Design.  Our Approach Ensures Smooth, Efficient Development Tailored To Your Needs While Integrating Robust Security Measures At Every Stage.
+                    </p>
+                    <div class="cta-container">
+                        <p>Get Your Project Estimation Just In A <br> Few Clicks</p>
+                    </div>
+                </div>
+            </section>
+
+
+            <section class="services" id="services">
+                <div class="container">
+                    <h2>SERVICES</h2>
+                    <p>At TDS, We Specialize In Delivering Tailored Solutions That Drive Measurable Results. Explore Our Areas Of Expertise And See How We Can Help Your Business Excel.</p>
+                    <div class="service-cards">
+                        <div class="card">
+                            <img src="assets/images/services/full_stack.png" alt="Full-Stack Development">
+                            <h3>Full-Stack Development</h3>
+                            <p>Bring Your Vision To Life With Our Full-Stack Development Services. We Specialize In Flutter, PHP Laravel, HTML, CSS, And JavaScript To Create Dynamic Web And Mobile Applications. Our UI/UX Designs Focus On User-Friendly Interfaces, Ensuring A Seamless Experience Across Devices. Whether It's A New App Or An Entire Platform, TDS Delivers Fast, Precise Solutions Tailored To Your Needs.</p>
+                        </div>
+                        <div class="card">
+                            <img src="assets/images/services/security.png" alt="DevSecOps">
+                            <h3>CyberSecurity</h3>
+                            <p>Our DevSecOps Approach Ensures That Security Is Embedded Throughout The Development Lifecycle. By Automating Security Checks And Integrating Them Into Your CI/CD Pipeline, We Help You Proactively Mitigate Risks Before They Impact Your Business.</p>
+                        </div>
+                        <div class="card">
+                            <img src="assets/images/services/cloud.png" alt="Cloud Solutions">
+                            <h3>Cloud Solutions</h3>
+                            <p>Seamlessly Transition Your Infrastructure To The Cloud With TDS. Our Cloud Migration Services Ensure A Smooth And Secure Shift To Platforms Like AWS And Azure. We Handle Everything—From Planning And Executing The Migration To Optimizing Your Cloud Environment For Performance And Scalability.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="additional-info">
+                <div class="info-content">
+                    <h3>Building Secure Digital Futures, One Solution at a Time.</h3>
+                    <a href="#" class="know-more-button left-button">Know More</a>
+                </div>
+            </section>
+
+            <section class="our-process" id="Process">
+                <h2>Our Process</h2>
+                <div class="process-steps">
+                    <div class="step">
+                        <div class="circle">1</div>
+                        <h3>Discovery & Strategy</h3>
+                        <p>We begin by understanding your business needs and defining a clear strategy that aligns with your goals.</p>
+                    </div>
+                    <div class="arrow">&#8594;</div>
+                    <div class="step">
+                        <div class="circle">2</div>
+                        <h3>Design & Development</h3>
+                        <p>Our team designs and develops innovative solutions tailored to your specific requirements.</p>
+                    </div>
+                    <div class="arrow">&#8594;</div>
+                    <div class="step">
+                        <div class="circle">3</div>
+                        <h3>Security & Testing</h3>
+                        <p>Every solution undergoes rigorous security checks and testing to ensure it meets the highest standards.</p>
+                    </div>
+                    <div class="arrow">&#8594;</div>
+                    <div class="step">
+                        <div class="circle">4</div>
+                        <h3>Deployment & Support</h3>
+                        <p>We deploy your solution with precision, and our ongoing support ensures your systems run smoothly post-launch.</p>
+                    </div>
+                </div>
+            </section>
+
+            <section class="additional-info">
+                <div class="info-content">
+                    <h3>Build great products with TDS Development Services.</h3>
+                    <a href="#" class="know-more-button left-button">Get Started</a>
+                </div>
+            </section>
+            <section class="Section-Why-Choose-US" id="ChooseUS">
+                <div class="Dev-Why-Choose-US">
+                    <h1 class="title-why-choose-us">Why Choose US?</h1>
+                    <p class="paragh-at-tds">At TDS, We Don't Just Deliver Services; We Build Long-Lasting Partnerships That Drive <br> Business Growth.</p>
+                    <div class="cards-us">
+                        <div class="card-second-section">
+                            <h3>Proven Expertise</h3>
+                            <p>With Years of experience across diverse industries, We bring deep technical knowledge and innovation to every project.</p>
+                        </div>
+                        <div class="card-second-section">
+                            <h3>Security-First Approach</h3>
+                            <p>Our DevSecOps practices ensure that Security is a top priority, protecting your business from cyber threats.</p>
+                        </div>
+                        <div class="card-second-section">
+                            <h3>Tailored Solutions</h3>
+                            <p>We Don’t Believe In One-Size-Fits-All. Every Solution We Create Is Customized To Meet Your Unique Business Needs.</p>
+                        </div>
+
+                        <div class="card-second-section">
+                            <h3>End-to-End Support</h3>
+                            <p>From Concept To Execution, We Support Your Digital Transformation Journey At Every Stage.</p>
+                        </div>
+                        <div class="card-second-section">
+                            <h3>Client-Centric</h3>
+                            <p>Your Success is our priority. We work closely with you to understand your goals and deliver solutions that exceed your expectations.</p>
+                        </div>
+                        <div class="card-second-section">
+                            <h3>Continuous Improvement</h3>
+                            <p>We’re Committed To Evolving And Optimizing Our Solutions To Ensure Long-Term Success And Growth.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <section class="our-services">
+                <h2>Full Stack, Cloud, Security</h2>
+                <p class="description">
+                    TDS Delivers Seamless Development And Robust Security Solutions Tailored To Drive Your Business Forward.
+                </p>
+                <div class="icons">
+                    <img src="assets/images/vector/group_icons.png" alt="Service Icons">
+                </div>
+                <button>
+                <span class="text">Get Started</span>
+                <svg
+                    width="34"
+                    height="34"
+                    viewBox="0 0 74 74"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <circle cx="37" cy="37" r="35.5" stroke="white" stroke-width="3"></circle>
+                    <path
+                    d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
+                    fill="white"
+                    ></path>
+                </svg>
+                </button>
+
+            </section>
+
+            <?php include 'form/contact-form.php'?>
+        </main>
+
+        <footer>
+
+        </footer>
+    </body>
+    <script src="script.js"></script>
+</html>
